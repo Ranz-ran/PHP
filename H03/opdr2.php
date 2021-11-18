05c5e3a6add77a430cbe94494abf14dac324ce56
Rita <ritakuo2002@gmail.com>
2021-11-18 11:42:48 +0100

    H03 opdr2 af

--- H03/opdr2.php
+++ H03/opdr2.php
@@ -2,7 +2,7 @@
 //Kerstboom
 echo "<div style='text-align: center'>";
 
-//Teken het aantal kaarsjes op de taart...
+//aantal * als boombladeren
 
 for($i = 0; $i < 9; $i++) {
     for ($ster = 0; $ster < $i; $ster++){
