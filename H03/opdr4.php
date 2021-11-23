c408fc56b8b64dc93220610ae2ab3898bc3a76b5
Rita <ritakuo2002@gmail.com>
2021-11-23 12:25:01 +0100

    H03 bezig 4

---
+++ H03/opdr4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP opdrcht</title>
+</head>
+<body>
+<?php
+    for($i = 1; $i <= 9; $i ++) {
+        echo "<img src= 'plaatje/aap".$i.".jpg'>";
+    }
+?>
+</body>
+</html>
