75c694e3a00476806fb0158803f8333b74c3c2a1
Rita <ritakuo2002@gmail.com>
2021-12-07 10:27:15 +0100

    H03 opdr4 af

--- H03/opdr4.php
+++ H03/opdr4.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PHP opdrcht</title>
+    <title>PHP opdrcht4</title>
+    <style>
+        body{
+            text-align: center;
+        }
+
+        .rood {
+            border: #e84949 solid 5px;
+        }
+/**/
+        .groen {
+            border: #4bd94b solid 5px;
+        }
+    </style>
 </head>
+
 <body>
 <?php
     for($i = 1; $i <= 9; $i ++) {
-        echo "<img src= 'plaatje/aap".$i.".jpg'>";
+        if($i % 2 == 0) {
+            $class = "class='rood'";
+        } else {
+            $class = "class='groen'";
+        }
+        echo "<img ".$class." src='plaatje/aap".$i.".jpg'>";
     }
 ?>
 </body>
