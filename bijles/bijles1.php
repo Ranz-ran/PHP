5e39e759346962ff3fdf430f5ebd5d72a25865fc
Rita <ritakuo2002@gmail.com>
2022-11-02 10:27:49 +0100

    Hw

--- bijles/bijles1.php
+++ bijles/bijles1.php
@@ -1,31 +1,126 @@
 <html>
     <head>
         <style>
-                * {font-size: 200%}
+            * {font-size: 200%;}
         </style>
     </head>
 </html>
+
 <?php
 
-    $namens = [
-      'Rita',
-      'Rory',
-      'Guillaume',
-      'Jorn',
-      'Joey'
-    ];
+/*
+* Maak een variabele met de naam 'achternaam' en de waarde 'Oosterink' en print die op het scherm.
+* Maak een variabele met de naam 'docenten' en daarin alle docenten van de opleiding.
+* Print alle namen van de docenten op het scherm
+* Print alle namen van de docenten op het scherm behalve die van Joey
+ */
 
-    Jsc($namens);
-    $namens[0];
-    $namens[1];
-    $namens[2];
+ 
 
-    for($i = 0; $i < 5; $i++){
-        echo $namens[$i];
+ 
+
+//variabelen
+
+//$lastName = "Orsel";
+
+//$rory = "Rory";
+
+//echo $lastName . "<br>";
+
+//echo "<br>";
+
+//echo $rory;
+
+ 
+
+$names = [
+
+    'Rita',
+
+    'Rory',
+
+    'Guillaume',
+
+    'Jorn',
+
+    'Joey'
+
+];
+
+//JSC($names);
+
+$names[0];
+
+$names[1];
+
+$names[2];
+
+//bepaal lengte van de array
+
+//echo count($names);
+
+//loop er doorheen
+
+for($i = 0; $i < 5; $i++){
+
+    if($names[$i] == 'Joey' || $names[$i] == 'Rita'){
+
+        if($names[$i] == "Rita"){
+
+            echo "Rita is de beste!" . "<br>";
+
+        } else {
+
+           
+
+        }
+
+    } else {
+
+        echo $names[$i] . " is geweldig!"; //$names[0] | $names[1] |
+
+        echo "<br>";
+
     }
 
-    function JSC ($input) {
-        echo "<pre>";
-        print_r($input);
-        echo
-    }
+   
+
+}
+
+//Stukje code met een naam dat uitgevoerd als je de naam aanroept. Functies kan je dus waarden meegeven.
+
+echo add(2, 3); //Rita-voorbeeld (met woef)
+
+function add($numberOne, $numberTwo){
+
+    $result = $numberOne + $numberTwo;
+
+    return $result;
+
+}
+
+function JSC($input){
+
+    echo "<pre>";
+
+    print_r($input);
+
+    echo "</pre>";
+
+}
+
+//array
+
+//
+
+//for loo
+
+//if else
+
+ 
+
+//functions
+
+ 
+
+//php specifieke dingen zoals $_POST
---
+++ bijles/huiswerk1.php
@@ -0,0 +1,15 @@
+<?php
+/*Huiswerk:
+Maak een array met 10 getallen (naam zelf kiezen van de array).
+Zet al die 10 getallen op het scherm.
+Tel al die 10 getallen op. En print het antwoord op het scherm.
+Schrijf een functie die twee waarden meekrijgt (voornaam en een achternaam) en print de hele naam op scherm.
+Schrijf een functie die een array als waarde meekrijgt en print alle waarden uit die array op het scherm.
+Schrijf code die op basis van de leeftijd (e.g. $age = 17) uitzoekt of iemand mag drinken. Als je ouder bent dan 18 wordt er op hey scherm geprint ‘Je mag drinken’. Anders: ‘’Je mag niet drinken’
+Maak een array met verschillende leeftijden.
+Maak een functie die uitzoekt of je mag drinken (kan je 6 voor hergebruiken)
+Laat de functie voor ieder van de waarde in de array uitzoeken of iemand mag drinken. Print dat op het scherm.
+Zoek vast uit wat $_POST is, wat cookies zijn en wat sessions zijn.
+Zoek vast uit hoe je verbinding kan maken met een database in PHP.
+*/
+
